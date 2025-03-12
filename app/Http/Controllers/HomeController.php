bbef33366d8026ec37fae22e74c51057dc8eb15b
wahyudestiyan <wahyudestiyan044@gmail.com>
2025-03-12 14:36:10 +0700

    routhome

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-use Illuminate\Support\Facades\Http;
 
 use Illuminate\Http\Request;
 
@@ -9,28 +8,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-         // Panggil API dari portal data
-    $response = Http::get('https://satudata.jatengprov.go.id/v1/data');
-    $data = $response->json();
-
-    // Pastikan data yang diterima adalah array
-    if (!is_array($data)) {
-        $data = [];
-    }
-
-    // Hitung total judul data
-    $totalJudul = count($data);
-
-    // Hitung jumlah data berdasarkan tahun_data
-    $dataPerTahun = [];
-    foreach ($data as $item) {
-        $tahun = $item['tahun_data'] ?? 'Unknown';
-        if (!isset($dataPerTahun[$tahun])) {
-            $dataPerTahun[$tahun] = 0;
-        }
-        $dataPerTahun[$tahun]++;
-    }
-
-    return view('home', compact('totalJudul', 'dataPerTahun'));
+        return view('home'); // Mengarahkan ke views/home.blade.php
     }
 }
