cc2fc4aa71a904e60a66146b203cfac7acc16bda
wahyudestiyan <wahyudestiyan044@gmail.com>
2025-03-13 10:32:04 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -83,9 +83,12 @@ Route::prefix('apibps')->group(function () {
 Route::post('/{apibpsId}/kirim', [ApiBpsController::class, 'kirimData'])->name('apibps.kirim');
 Route::get('/export-api-bps', [ApiBpsController::class, 'exportApiBps'])->name('export.api.bps');
 Route::get('/export-pdf', [ApiBpsController::class, 'exportPdfBps'])->name('apibps.export-pdf');
-Route::delete('{id}', [ApiBpsController::class, 'destroy'])->name('apibps.destroy');
 
 });
+
+
+Route::delete('/apibps/{id}', [ApiBpsController::class, 'destroy'])->name('apibps.destroy');
+
 // Route::post('/apibps/{id}/mapping', [ApiBpsController::class, 'storeMapping'])->name('apibps.mapping.store');
 
 
