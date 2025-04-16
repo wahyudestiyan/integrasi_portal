5ed14e6e5be4353c9d61f6740b11f37955e26d66
Didi Wijaya <ddwijaya3@gmail.com>
2025-04-16 02:05:59 +0000

    update

--- start_frankenphp.sh
+++ start_frankenphp.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-php artisan octane:frankenphp --port=8082 --host=0.0.0.0
+#php artisan octane:frankenphp --port=8000 --host=0.0.0.0
+#php artisan serve --port=8000 --host=0.0.0.0
+php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000
