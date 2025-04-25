20899087a7074dfc5a96b6b46439a623ac40f241
wahyudestiyan <wahyudestiyan044@gmail.com>
2025-04-25 09:12:07 +0700

    update judul blade

--- resources/views/pemeriksaan/lihat_judul.blade.php
+++ resources/views/pemeriksaan/lihat_judul.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 <div class="container">
-    <h4 class="mb-4">Daftar Judul Lengkap</h4>
+    <h4 class="mb-4">Daftar Detail Pemeriksaan</h4>
+
     <div class="table-responsive">
         <table class="table table-bordered table-striped table-hover align-middle">
             <thead class="table-dark text-center">
@@ -26,15 +27,21 @@
                             </span>
                         </td>
                         <td>{{ $data->keterangan }}</td>
-
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="4" class="text-center">Tidak ada data yang tersedia.</td>
+                        <td colspan="5" class="text-center">Tidak ada data yang tersedia.</td>
                     </tr>
                 @endforelse
             </tbody>
         </table>
+    </div>
+
+    {{-- Tombol Kembali --}}
+    <div class="mt-4">
+        <a href="{{ url()->previous() }}" class="btn btn-secondary">
+            <i class="bi bi-arrow-left"></i> Kembali
+        </a>
     </div>
 </div>
 @endsection
