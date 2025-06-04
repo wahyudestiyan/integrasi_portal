659c106e567e5b507a588ba20758d00eca5cf183
wahyudestiyan <wahyudestiyan044@gmail.com>
2025-06-04 14:16:13 +0700

    export edit

--- app/Exports/DataMonitoringExport.php
+++ app/Exports/DataMonitoringExport.php
@@ -2,11 +2,12 @@
 
 namespace App\Exports;
 
-use App\Models\DataApi; // Pastikan model yang digunakan sesuai
+use App\Models\DataApi;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class DataMonitoringExport implements FromCollection, WithHeadings
+class DataMonitoringExport implements FromCollection, WithHeadings, WithMapping
 {
     protected $instansiId;
 
@@ -16,24 +17,34 @@ class DataMonitoringExport implements FromCollection, WithHeadings
     }
 
     /**
-     * Mengambil data berdasarkan instansi
+     * Ambil data dari database
      */
     public function collection()
     {
-        // Menggunakan instansi_token_id untuk filter data
         return DataApi::where('instansi_token_id', $this->instansiId)
             ->select('id_api', 'judul', 'tahun_data')
             ->get();
     }
-    
 
     /**
-     * Menambahkan heading (judul kolom) di file Excel
+     * Ubah tampilan setiap baris sebelum diekspor
+     */
+    public function map($row): array
+    {
+        return [
+            'https://satudata.jatengprov.go.id/v1/data/' . $row->id_api,
+            $row->judul,
+            $row->tahun_data,
+        ];
+    }
+
+    /**
+     * Judul kolom di Excel
      */
     public function headings(): array
     {
         return [
-            'ID API',
+            'Link API',
             'Judul',
             'Tahun Data',
         ];
